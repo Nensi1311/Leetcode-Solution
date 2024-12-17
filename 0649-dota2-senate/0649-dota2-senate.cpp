4712fe6c8254a8778c3fc3613bc4a74899a30200
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-12-17 19:11:03 +0530

    Time: 4 ms (44.26%), Space: 11.4 MB (22.36%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> r, d;
+        int n = senate.size();
+
+        for(int i=0; i<n; i++){
+            if(senate[i]=='R'){
+                r.push(i);
+            }
+            else{
+                d.push(i);
+            }
+        }
+
+        while(!r.empty() && !d.empty()){
+            if(r.front()<d.front()){
+                r.pop();
+                r.push(n++);
+                d.pop();
+            }
+            else{
+                d.pop();
+                d.push(n++);
+                r.pop();
+            }
+        }
+        return (r.empty()? ("Dire"):("Radiant"));
+    }
+};
